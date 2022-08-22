bdd397c9bc723dbf2e921ebcb941d41022f8ae25
Arsalanbashir831 <arsalanbashir831@gmail.com>
2022-08-22 21:53:21 +0500

    comit

--- order-page/src/Layout.jsx
+++ order-page/src/Layout.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Box from '@mui/material/Box';
 
 const Layout = () => {
-
+let bgColor=getComputedStyle(document.documentElement).getPropertyValue("--backgroundColor");
     return (
-
         <>
            <div className="Container">
 
-                <Box  sx={{ border: '1px dashed grey', backgroundColor: 'primary.main', margin: '5%' }} >
+                <Box  sx={{ border: '1px dashed grey', backgroundColor: `${bgColor}`, margin: '5%' }} >
                 Arsalan 
                 </Box>
            </div>
