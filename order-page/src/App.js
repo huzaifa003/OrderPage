6a975e4f467b2444b1b4656abcc8324e1e8e1487
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-22 19:57:55 +0500

    Fixed a bug

--- order-page/src/App.js
+++ order-page/src/App.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <div className = 'container'>
+    <div className = 'Container'>
       <Box sx={{ border: '1px dashed grey', backgroundColor: 'primary.main', margin: '5%' }}>
         Gekki
       </Box>
