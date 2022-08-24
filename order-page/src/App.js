b100f38cd34832e83e4a9b781949bc422c857731
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-24 20:34:32 +0500

    Copied the elements

--- order-page/src/App.js
+++ order-page/src/App.js
@@ -2,6 +2,7 @@
 import Layout from './Layout';
 import Header from './Header'
 import headerLogo from './assets/icons/logo_header.845e5c5.svg'
+import SmallMobileCategories from './SmallMobileCategories';
 function App() {
   return (
     <>
@@ -11,7 +12,7 @@ function App() {
         <img alt='Logo' style={{position: 'fixed', marginTop: '1.5%' , marginLeft: '5%',zIndex:'10'}} src = {headerLogo}></img>
 
         <Layout/>
-        
+        <SmallMobileCategories/>
       </div>
     </>
   );
---
+++ order-page/src/SmallMobileCategories.jsx
@@ -0,0 +1,13 @@
+import './SmallMobileCategroies.css'
+const SmallMobileCategories = () => {
+    <div pid="19" class="product default selected"><div class="bestOffer"><p>-70% OFF TODAY</p></div> <div class="sold"><p>
+        Last Sold:
+        <span>22s ago</span></p></div> <div class="productSelect"><div class="checkbox"></div> <p class="name">12 Months Pass</p></div> <p class="price">
+            3.99<span>€</span></p> <p class="billingcycle">per month</p> <div class="save"><p>
+                Save 70%
+            </p></div> <p class="guarantee">
+            30-day money-back guarantee
+        </p></div>
+}
+
+export default SmallMobileCategories
