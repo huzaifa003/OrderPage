93cea3264db2ded900afe3c2fd69f964496f2cd0
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-25 15:04:39 +0500

    Added theme

--- order-page/src/App.js
+++ order-page/src/App.js
@@ -8,21 +8,33 @@ import CssBaseline from '@mui/material/CssBaseline';
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      paper: '#030e2e',
+    },
+    text: {
+      primary: '#ffffff',
+    },
   },
 });
 function App() {
   return (
     <>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <div className="Container">
-        
-        <Header/>
-        <img alt='Logo' style={{position: 'fixed', marginTop: '1.5%' , marginLeft: '5%',zIndex:'10'}} src = {headerLogo}></img>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <div className="Container">
 
-        <Layout/>
-        
-      </div>
+          <Header />
+          <img alt='Logo' style={{ position: 'fixed', marginTop: '1.5%', marginLeft: '5%', zIndex: '10' }} src={headerLogo}></img>
+
+          <Layout />
+
+        </div>
       </ThemeProvider>
     </>
   );
