e64d4d0c1adf476dd2a8b41fbc8a35e5ae50de20
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-25 17:19:09 +0500

    Added Selection

--- order-page/src/Devices.jsx
+++ order-page/src/Devices.jsx
@@ -8,8 +8,24 @@ import DevicesCard from './DevicesCard'
 import Grid from '@mui/material/Grid';
 
 export default function SimpleAccordion() {
+  const [activatedId, setactivatedId] = React.useState('d1');
+  const clickHandler = (id) => {
+    console.log(activatedId)
+    const element_prev = document.getElementById(activatedId);
+
+    const element = document.getElementById(id);
+
+    if (element_prev === null) {
+      setactivatedId(id);
+      element.classList.add('selectedDiv')
+    } else {
+      element_prev.classList.remove('selectedDiv');
+      setactivatedId(id);
+      element.classList.add('selectedDiv')
+    }
 
 
+  }
   return (
     <div>
       <Accordion>
@@ -18,23 +34,29 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography> Devices</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Grid sx={{ flexGrow: 1 }} container spacing={2}>
             <Grid item xs={12}>
               <Grid container justifyContent="left" spacing={2}>
-             
-                  <Grid key={0} item>
-                    <DevicesCard id= {0} noOfDevice = {1} price = {5}></DevicesCard>
-                  </Grid>
-                  <Grid key={1} item>
-                    <DevicesCard id= {1} noOfDevice = {2} price = {10}></DevicesCard>
-                  </Grid>
-                  <Grid key={2} item>
-                    <DevicesCard id= {2} noOfDevice = {3} price = {15}></DevicesCard>
-                  </Grid>
-          
+
+                <Grid key={0} item>
+                  <div onClick={() => clickHandler('d1')}>
+                    <DevicesCard divId='d1' noOfDevice={1} price={5} activatedId={activatedId}></DevicesCard>
+                  </div>
+                </Grid>
+                <Grid key={1} item>
+                  <div onClick={() => clickHandler('d2')} >
+                    <DevicesCard divId={'d2'} noOfDevice={2} price={10} activatedId={activatedId}></DevicesCard>
+                  </div>
+                </Grid>
+                <Grid key={2} item>
+                  <div onClick={() => clickHandler('d3')}>
+                    <DevicesCard divId={'d3'} noOfDevice={3} price={15} activatedId={activatedId}></DevicesCard>
+                  </div>
+                </Grid>
+
               </Grid>
             </Grid>
           </Grid>
--- order-page/src/DevicesCard.jsx
+++ order-page/src/DevicesCard.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+
+let activatedColor = getComputedStyle(document.documentElement).getPropertyValue("--packageActive");
 const bull = (
     <Box
         component="span"
@@ -18,31 +18,35 @@ const bull = (
 
 export default function OutlinedCard(props) {
     return (
-        <Box sx={{ minWidth: 275,  }}>
-            <CardActionArea>
-            <Card variant="outlined">
-                <React.Fragment>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            No Of Device(s)
-                        </Typography>
-                        <Typography variant="h5" component="div">
-                            {props.noOfDevice} Devices
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            Price: {`+$${props.price}` } {bull}
-                        </Typography>
-                        <Typography variant="body2">
-                            Enjoy and Use on
-                            <br />
-                            {`${props.noOfDevice} of your devices`}
-                        </Typography>
-                    </CardContent>
-            
-                </React.Fragment>
+        <div id={props.divId}>
 
-            </Card>
-            </CardActionArea>
-        </Box>
+            
+            <Box sx={{ minWidth: 275, }}>
+                <CardActionArea>
+                    <Card   sx={(props.activatedId===props.divId)? {color: 'white', border: 2 , borderColor: activatedColor, borderRadius: 1, borderWidth: 2} : {color:'rgba(255, 255, 255, 0.5)'}} raised='true' variant="outlined">
+                        <React.Fragment>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    No Of Device(s)
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    {props.noOfDevice} Devices
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    Price: {`+$${props.price}`} {bull}
+                                </Typography>
+                                <Typography variant="body2">
+                                    Enjoy and Use on
+                                    <br />
+                                    {`${props.noOfDevice} of your devices`}
+                                </Typography>
+                            </CardContent>
+
+                        </React.Fragment>
+
+                    </Card>
+                </CardActionArea>
+            </Box>
+        </div>
     );
 }
